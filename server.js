a3b504329a8def40c311690815771d56a056242b
XY <3400164864@qq.com>
2022-06-28 07:41:44 +0800

    包结构目录

---
+++ server.js
@@ -0,0 +1,32 @@
+//引入第三方插件
+const express = require('express')
+const app = express()
+const body_parse = require('body-parser')
+const path = require('path')
+const cookieParse = require('cookie-parser')
+const session = require('express-session')
+const axios = require('axios')
+
+// 配置
+app.engine('html', require('express-art-template'))
+app.use(body_parse.urlencoded({ extended: false }))
+app.use(body_parse.json())
+app.use(session({
+    secret: 'hubwizApp',
+    cookie: { maxAge: 60 * 1000 * 30 * 24 },
+    resave: true,
+    saveUninitialized: false,
+  }))
+app.use('/public', express.static(path.join(__dirname, './static')))
+
+// 引入路由
+const sAdminRouter=require('./router/sAdminRouter')
+
+// 配置路由
+app.use(sAdminRouter)
+
+// 启动服务器
+app.listen(8080,()=>{
+    console.log('服务器已启动,端口号:8080')
+})
+
