cea342cf853a9dd948a4e5cd94044a3a844741bc
郑龙龙 <10434542+zheng-longlong1@user.noreply.gitee.com>
2022-06-29 19:21:00 +0800

    进度

--- router/userRouter.js
+++ router/userRouter.js
@@ -4,6 +4,12 @@ const router = express.Router();
 router.get('/history',(req,res)=>{
     res.render('history.html');
 })
+router.get('/adminUsers',(req,res)=>{
+    res.render('adminUsers.html');
+})
 
+router.get('/progress',(req,res)=>{
+    res.render('progress.html');
+})
 
 module.exports = router;
