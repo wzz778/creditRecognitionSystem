e7d2f1740cab41ab4e61ee2758de2cfaf7f9a2c3
郑龙龙 <10434542+zheng-longlong1@user.noreply.gitee.com>
2022-07-01 20:24:51 +0800

    编写接口

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,47 +3,62 @@ const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 axios.default.baseURL = 'http://110.40.205.103:8099/';
-function ribbon(req,res,next){
-    axios.default.headers = req.session.token;
-    next();
-}
-router.get('/history',ribbon,(req,res)=>{
+router.get('/history',(req,res)=>{
+    console.log(req.session.token);
     // let {nodePage,pageSize} = req.query;
-    axios({
-        method:'get',
-        url:'user/records',
-        params:{
-            nodePage:1,
-            pageSize:10,
-        },
-    }).then((data)=>{
-        console.log(data);
-        // res.send(data);
-    }).catch((err)=>{
-
-    })
     res.render('history.html')
 })
-router.get('/adminUsers',ribbon,(req,res)=>{
-    axios({
-        method:'get',
-        url:'admin/commonUserPage',
-        params:{
-            nodePage:1,
-            nodeSize:10
-        }
-    }).then((data)=>{
-        console.log(data);
-    }).catch((err)=>{
-        console.log(err);
-    })
+router.get('/adminUsers',(req,res)=>{
+
     res.render('adminUsers.html');
 })
 
-router.get('/progress',ribbon,(req,res)=>{
+router.get('/progress',(req,res)=>{
     res.render('progress.html');
 })
 router.get('/particulars',(req,res)=>{
     res.render('particulars.html');
 })
+
+
+router.get('/users/records',(req,res)=>{
+    // let {nodePage,pageSize} = req.body;
+    axios({
+        method:'get',
+        url:'/user/records',
+        params:req.body,
+        headers:req.session.token,
+    }).then((date)=>{
+        res.send(date);
+    }).catch((err)=>{
+        res.send(err);
+    })
+})
+
+router.get('/user/application',(req,res)=>{
+    axios({
+        method:'get',
+        url:'/user/application',
+        params:req.body,
+        headers:req.session.token,
+    }).then((date)=>{
+        res.send(date);
+    }).catch((err)=>{
+        res.send(err);
+    })
+})
+
+router.get('/admin/commonUserPage',(req,res)=>{
+    axios({
+        method:'get',
+        url:'/user/commonUserPage',
+        params:req.body,
+        headers:req.session.token,
+    }).then((date)=>{
+        res.send(date);
+    }).catch((err)=>{
+        res.send(err);
+    })
+})
+
 module.exports = router;
--- static/js/history.js
+++ static/js/history.js
@@ -35,3 +35,23 @@ layui.use('laydate',function () {
         // }
     })
 })
+
+function package(method,url,args,callback){
+    if(method == "post"){
+        $.post(url,args,function (data,status){
+            callback(data,status);
+        })
+    }else{
+        $.get(url,args,function (data,status){
+            callback(data,status);
+        })
+    }
+}
+
+
+function render(numbers){
+    package('get','/users/records',{nodePage:numbers,pageSize:10},function (data){
+        console.log(data);
+    })
+}
+render(1);
