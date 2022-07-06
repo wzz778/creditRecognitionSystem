4b88fc3a3cb34408cca3bdf9ecbb13224cf2d38f
郑龙龙 <10434542+zheng-longlong1@user.noreply.gitee.com>
2022-07-06 22:14:54 +0800

    交互

--- static/js/history.js
+++ static/js/history.js
@@ -1,20 +1,20 @@
 
 
-layui.use('laypage',function (){
-    var laypage = layui.laypage;
-
-    laypage.render({
-        elem:'page',
-        count:100,
-        jump:function (obj,first){
-            console.log(obj.curr);
-            console.log(obj.limit)
-            if(!first){
-
-            }
-        }
-    })
-})
+// layui.use('laypage',function (){
+//     var laypage = layui.laypage;
+//
+//     laypage.render({
+//         elem:'page',
+//         count:100,
+//         jump:function (obj,first){
+//             console.log(obj.curr);
+//             console.log(obj.limit)
+//             if(!first){
+//
+//             }
+//         }
+//     })
+// })
 layui.use('laydate',function () {
     var laydate = layui.laydate;
 
@@ -38,6 +38,8 @@ layui.use('laydate',function () {
     })
 })
 
+
+
 let main_content = document.getElementsByClassName('main-content');
 let  student_operator = document.getElementsByClassName('student-operator');
 let btn = document.getElementsByClassName('btn');
@@ -46,6 +48,8 @@ let search = document.getElementsByClassName('search');
 let startTime = document.getElementById('startTime');
 let endTime = document.getElementById('endTime');
 let check = document.getElementsByClassName('check');
+let layui_btn = document.getElementsByClassName('layui-btn');
+let cover_layer = document.getElementsByClassName('cover-layer')
 
 function package(method,url,args,callback){
     if(method == "post"){
@@ -84,37 +88,143 @@ function pading(sum){
 
 
 function render(numbers){
-    let index = search_type[0].selectedIndex;
-    let typeName = search_type[0].options[index].value;
     let index1 = search_type[1].selectedIndex;
+    let index = search_type[0].selectedIndex;
+    let his = {approval_status:search_type[1].options[index1].value,
+        b_Indicator_name:search_type[0].options[index].value,
+        b_points_available:search[0].value,
+        beginDate:startTime.value,
+        endDate: endTime.value,
+        nodePage:numbers,
+        pageSize:10,
+    }
+    // let index = search_type[0].selectedIndex;
+    let typeName = search_type[0].options[index].value;
+    if(typeName == ''){
+        delete  his.b_Indicator_name
+    }
+
+    // let index1 = search_type[1].selectedIndex;
     let typeStatus = search_type[1].options[index1].value;
+    if(typeStatus == ''){
+        delete  his.approval_status
+    }
     let scords = search[0].value;
     let start = startTime.value;
     let end = endTime.value;
+    if(scords == ''){
+        delete  his.b_points_available
+    }
+    if(start == ''){
+        delete his.beginDate
+    }
+    if(end == ''){
+        delete  his.endDate
+    }
     console.log(typeName);
     console.log(typeStatus);
-    package('get','/users/records',{approval_status:typeStatus,b_Indicator_name:typeName,b_points_available:scords,beginDate:start,endDate:end,nodePage:numbers,pageSize:10},function (data){
-        console.log(data);
-
-
-
-
-        let html = template('main-content',data);
-        main_content[0].innerHTML = html;
+    console.log(his);
+    // package('get','/users/records',{approval_status:typeStatus,b_Indicator_name:typeName,b_points_available:scords,beginDate:start,endDate:end,nodePage:numbers,pageSize:10},function (data){
+    //     console.log(data.data);
+    //
+    //
+    //
+    //
+    //     let html = template('main-content',data.data);
+    //     main_content[0].innerHTML = html;
+    //     let index = 0;
+    //     for(let i=0;i<check.length;i++){
+    //         check[i].ids = '';
+    //         check[i].onclick = function () {
+    //
+    //
+    //             window.location.href = 'particulars.html?id=' + check[i].ids;
+    //         }
+    //     }
+    // })
+    axios({
+        method:'get',
+        url:'/users/records',
+        params:his,
+    }).then((data)=>{
+        console.log(data.data.data.allRecords);
+        let date = data.data.data.allRecords;
+        let all = "";
+        // let html = template('main-content',data.data);
+        for(let i=0;i<date.length;i++){
+            if(date[i].application.approval_status == 0){
+                var approval_status = '未审核';
+            }else if(date[i].application.approval_status == 1){
+                var approval_status = '通过';
+            }else{
+                var approval_status = '未通过';
+            }
+            all +=`<ul class="header">
+                        <li class="student-name lis">${date[i].application.user.name}</li>
+                        <li class="student-major lis">${date[i].application.user.academy}</li>
+                        <li class="student-class lis">${date[i].application.user.major_class}</li>
+                        <li class="student-apply lis">${date[i].application.creditType.afirstLevel}</li>
+                        <li class="student-time lis">${date[i].application.application_time}</li>
+                        <li class="student-state lis">${approval_status}</li>
+                        <li class="student-apply-credit lis">${date[i].application.classify.b_points_available}</li>
+                        <li class="student-operator lis"><span class="check">查看</span></li>
+                    </ul>`
+        }
+        main_content[0].innerHTML = all;
         let index = 0;
-        for(let i=0;i<check.length;i++){
-            check[i].ids = '';
+        // pading(10);
+        for(let i=0;i<check.length;i++) {
+            check[i].ids = date[i].application.id;
             check[i].onclick = function () {
 
 
-                window.location.href = 'particulars.html?id=' + check[i].ids;
+                window.location.href = 'particulars?id=' + check[i].ids;
             }
         }
     })
 }
+render(1);
+
+
 
 btn[0].onclick = function (){
     render(1);
+    // axios({
+    //     method:'get',
+    //     url:'/users/records',
+    //     params:{
+    //         beginDate:startTime.value,
+    //         endDate:endTime.value,
+    //         nodePage:1,
+    //         pageSize:10}
+    // }).then((data)=>{
+    //     console.log(data.data.data.allRecords);
+    //     let date = data.data.data.allRecords;
+    //     let all = "";
+    //     // let html = template('main-content',data.data);
+    //     for(let i=0;i<date.length;i++){
+    //         all +=`<ul class="header">
+    //                     <li class="student-name lis">${date[i].application.user.name}</li>
+    //                     <li class="student-major lis">${date[i].application.user.academy}</li>
+    //                     <li class="student-class lis">${date[i].application.user.major_class}</li>
+    //                     <li class="student-apply lis">${date[i].application.creditType.afirstLevel}</li>
+    //                     <li class="student-time lis">${date[i].application.application_time}</li>
+    //                     <li class="student-state lis">${date[i].application.approval_status}</li>
+    //                     <li class="student-apply-credit lis">${date[i].application.specific_information}</li>
+    //                     <li class="student-operator lis"><span class="check">查看</span></li>
+    //                 </ul>`
+    //     }
+    //     main_content[0].innerHTML = all;
+    //     let index = 0;
+    //     for(let i=0;i<check.length;i++) {
+    //         check[i].ids = date[i].application.id;
+    //         check[i].onclick = function () {
+    //
+    //
+    //             window.location.href = 'particulars?id=' + check[i].ids;
+    //         }
+    //     }
+    // })
 
 }
 btn[1].onclick = function (){
@@ -127,4 +237,20 @@ btn[1].onclick = function (){
 }
 
 
-render(1);
+// layui_btn[0].addEventListener('click',function (){
+//     cover_layer[0].style.display = 'none';
+// })
+
+layui.use('form', function(){
+    var form = layui.form;
+
+    //监听提交
+    form.on('submit(formDemo)', function(data){
+        console.log(data);
+    });
+});
+
+
+
+
+
--- static/js/particulars.js
+++ static/js/particulars.js
@@ -16,6 +16,7 @@ function pic(){
 }
 
 let id = window.location.search.split("=")[1];
+console.log(id);
 let history_details = document.getElementsByClassName('history-details')
 
 function package(method,url,args,callback){
@@ -31,25 +32,75 @@ function package(method,url,args,callback){
 }
 
 function render(id){
-    package('get','/admin/oneApplication',{id:id},function (data){
-        console.log(data);
-        let html = template('details',data);
-        history_details[0].innerHTML = html;
-        pic();
-    })
-    // axios({
-    //     method:'get',
-    //     url:'/admin/oneApplication',
-    //     data:{
-    //         id:id,
-    //     }
-    // }).then((data)=>{
+    // package('get','/admin/oneApplication',{id:id},function (data){
     //     console.log(data);
     //     let html = template('details',data);
     //     history_details[0].innerHTML = html;
     //     pic();
-    // }).catch((err)=>{
-    //     console.log(err);
     // })
+    axios({
+        method:'get',
+        url:'/admin/oneApplication',
+        params:{
+            id:id,
+        }
+    }).then((data)=>{
+        console.log(data.data.data);
+        let date = data.data.data;
+        // let html = template('details',data);
+        let all = `<div class="application-message">
+                        <ul class="messages">
+                            <li>
+                                姓名：
+                                <span class="name">${date.user.name}</span>
+                            </li>
+                            <li>
+                                性别：
+                                <span class="sex">${date.user.sex}</span>
+                            </li>
+                            
+                            <li>
+                                所属学院：
+                                <span>${date.user.academy}</span>
+                            </li>
+                            
+                            <li>
+                                班级：
+                                <span>${date.user.major_class}</span>
+                            </li>
+                            <li>
+                                申请类型：
+                                <span>${date.creditType.afirstLevel}</span>
+                            </li>
+                            <li>
+                                是否为集体项目：
+                                <span>否</span>
+                            </li>
+                            <li class="parctice-content">
+                                实践内容说明：
+                                <div>
+
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="attchment">
+                        <div class="attchment-title">附件</div>
+                        <div class="layui-carousel" id="attchment-pic">
+                            <div carousel-item>
+                                <div class="attchment-everyOne"><img src='/public/img/loginback.png' class="attchment-imgs"></div>
+                                <div class="attchment-everyOne"><img src='/public/img/loginback.png' class="attchment-imgs"></div>
+                                <div class="attchment-everyOne"><img src='/public/img/loginback.png' class="attchment-imgs"></div>
+                                <div class="attchment-everyOne"><img src='/public/img/loginback.png' class="attchment-imgs"></div>
+                                <div class="attchment-everyOne"><img src='/public/img/loginback.png' class="attchment-imgs"></div>
+                            </div>
+                        </div>
+                    </div>`
+        history_details[0].innerHTML = all;
+        pic();
+    }).catch((err)=>{
+        console.log(err);
+    })
 }
 render(id);
